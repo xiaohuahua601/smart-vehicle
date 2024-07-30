c07ee966bc881094a3f9807a239692da6073f48f
赵微 <zhaowei3@tedu.cn>
2024-07-30 14:19:27 +0800

    111

---
+++ src/main/java/cn/tedu/smart/vehicle/SmartVehicleApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.smart.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartVehicleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartVehicleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/smart/vehicle/SmartVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.smart.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartVehicleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
