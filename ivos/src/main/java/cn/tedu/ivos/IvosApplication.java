422a283fe4a18bc8dde01b025f30ae91137de24e
赵微 <zhaowei3@tedu.cn>
2024-08-02 21:55:09 +0800

    后端项目创建

---
+++ ivos/src/main/java/cn/tedu/ivos/IvosApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.ivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvosApplication.class, args);
+    }
+
+}
